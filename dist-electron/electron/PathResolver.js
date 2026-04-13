2682693fc61081fa7bb17d779e3a811ffac03544
Shivam Jha <shivam23040223@gmail.com>
2026-04-13 13:15:55 +0530

    Removed dist-electron from tracking

--- dist-electron/electron/PathResolver.js
+++
@@ -1,5 +0,0 @@
-import path from "path";
-import { app } from "electron";
-export const getPreloadPath = () => {
-    return path.join(app.getAppPath(), "dist-electron", "electron", "preload.cjs");
-};
